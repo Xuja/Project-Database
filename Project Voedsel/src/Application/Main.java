f517e5fb2488dead7a7ff4d2a9620d156d136dba
Xu <xwjl92@hotmail.com>
2017-05-22 12:32:35 +0200

    as

---
+++ Project Voedsel/src/Application/Main.java
@@ -0,0 +1,12 @@
+package Application;
+
+import Display.Display;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Display display = new Display("Voedselbank", 800, 600);
+	}
+
+}
